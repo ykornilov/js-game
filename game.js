f3c0650bcfebda4846b69906813ffe2393459454
Yury Kornilov <kornilovyv81@gmail.com>
2017-08-14 17:50:52 +0300

    added class Vector

--- game.js
+++ game.js
@@ -1,1 +1,23 @@
 'use strict';
+
+class Vector {
+	constructor(x = 0, y = 0) {
+		this.x = x;
+		this.y = y;
+	}
+
+	plus(vector) {
+		if (!(vector instanceof Vector)) {
+			throw new Error('Можно прибавлять к вектору только вектор типа Vector');
+		}
+
+		return new Vector(this.x + vector.x, this.y + vector.y);
+	}
+
+	times(num) {
+		return new Vector(this.x * num, this.y * num);
+	}
+}
+
+
+
